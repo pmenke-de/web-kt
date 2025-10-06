0b27c03c1450aca0bf3dc0d8f22c78df3a500b18
pmenke <info@pmenke.de>
2025-10-06 19:02:33 +0200

    add logging util for coroutine scopes
    Signed-off-by: pmenke <info@pmenke.de>

---
+++ src/commonMain/kotlin/de/pmenke/webkt/log/LoggingErrorHandler.kt
@@ -0,0 +1,11 @@
+package de.pmenke.webkt.log
+
+import kotlinx.coroutines.CoroutineExceptionHandler
+
+/**
+ * Creates a CoroutineExceptionHandler that logs unhandled exceptions using the provided logger.
+ */
+@Suppress("FunctionName")
+fun LoggingErrorHandler(logger: Logger): CoroutineExceptionHandler = CoroutineExceptionHandler { context, throwable ->
+    logger.error { "unhandled exception in coroutine-context $context: $throwable\n${throwable.printStackTrace()}" }
+}
