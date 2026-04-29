25c9f83ce51fdf2fb51b1e496946af6df453a12d
pmenke <philipp.menke@global-office.de>
2026-04-29 16:37:13 +0200

    expose changes on hash to support navigation updates

--- src/commonMain/kotlin/de/pmenke/webkt/services/NavigatorService.kt
+++ src/commonMain/kotlin/de/pmenke/webkt/services/NavigatorService.kt
@@ -26,12 +26,19 @@ private val LOG = Logger("de.pmenke.webkt.services.NavigatorService")
 class NavigatorService {
     private val basePath = URL(document.baseURI).pathname.trimEnd('/')
     private var pathMut = MutableStateFlow(window.location.pathname.removePrefix(basePath))
+    private var hashMut = MutableStateFlow(window.location.hash)
 
     /**
      * The current path, starting with a '/' and without the base path (if any).
      * Updates on browser navigation (a-click, back/forward buttons) as well as programmatic navigation via [navigateTo].
      */
     val path get() = pathMut.asStateFlow()
+
+    /**
+     * The current URL hash (including the leading '#'), or an empty string if no hash is present.
+      * Updates on browser navigation (a-click, back/forward buttons) as well as programmatic navigation via [navigateTo].
+      */
+    val hash get() = hashMut.asStateFlow()
 
     init {
         LOG.debug("NavigatorService created", aspect = LoggingAspect.LIFECYCLE)
@@ -46,13 +53,17 @@ class NavigatorService {
         }
         // handle browser navigation (back/forward)
         window.addEventListener("popstate") { event ->
-            val state = (event as PopStateEvent).state?.unsafeCast<JsObject>()
+            if (event !is PopStateEvent) return@addEventListener
+            val state = event.state?.unsafeCast<JsObject>()
             if (state != null) {
                 val toPath = state["path"].toString()
                 LOG.debug("popstate navigated to $toPath")
                 pathMut.value = toPath.removePrefix(basePath)
+                hashMut.value = state["hash"]?.toString() ?: ""
             } else {
-                LOG.warn("popstate event without state", event)
+                LOG.debug("popstate event without state", event)
+                pathMut.value = window.location.pathname.removePrefix(basePath)
+                hashMut.value = window.location.hash
             }
         }
     }
@@ -60,29 +71,32 @@ class NavigatorService {
     /**
      * Navigate to the given [path].
      */
-    fun navigateTo(path: String) {
+    fun navigateTo(path: String, hash: String = "") {
         val fullPath = if (path.startsWith("/")) {
             basePath + path
         } else {
             "$basePath/$path"
         }
         LOG.debug("programmatic navigation to $fullPath")
-        navigateToFullPath(fullPath)
+        navigateToFullPath(fullPath, hash)
     }
 
-    private fun navigateToFullPath(path: String) {
-        val state = mapOf("path" to path).toJsAny()
-        window.history.pushState(state, "", path)
+    private fun navigateToFullPath(path: String, hash: String) {
+        val state = mapOf("path" to path, "hash" to hash).toJsAny()
+        window.history.pushState(state, "", path + hash)
+        window.location.hash = hash
         pathMut.value = path.removePrefix(basePath)
+        hashMut.value = hash
     }
 
     private fun onNavigate(event: Event, target: EventTarget?, href: String) {
         if (href.startsWith(window.location.origin)) {
-            val targetPath = href.substring(window.location.origin.length)
+            val url = URL(href)
+            val targetPath = url.pathname
             event.preventDefault()
             if ("$basePath/${path.value}" != targetPath) {
                 LOG.debug("onclick navigation to $targetPath")
-                navigateToFullPath(targetPath)
+                navigateToFullPath(targetPath, url.hash)
             }
         }
     }
